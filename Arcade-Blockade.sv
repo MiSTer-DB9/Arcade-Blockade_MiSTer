e28bb34793d9e9b9a57f949725d0265601c7aab2
Timothy Redaelli <timothy.redaelli@gmail.com>
2026-06-09 15:34:36 +0200

    DB9: consume programmable remap matrix at joystick merge

--- Arcade-Blockade.sv
+++ Arcade-Blockade.sv
@@ -336,8 +336,8 @@ wire [15:0] joystick_0_USB, joystick_1_USB;
 wire [15:0] joystick_2, joystick_3;
 
 // [MiSTer-DB9-Pro BEGIN] - DB controllers muted while OSD is open
-wire [31:0] joystick_0 = joydb_1ena ? (OSD_STATUS ? 32'b0 : {1'b0,joydb_1[11],joydb_1[10],joydb_1[4:0]}) : joystick_0_USB;
-wire [31:0] joystick_1 = joydb_2ena ? (OSD_STATUS ? 32'b0 : {1'b0,joydb_2[11],joydb_2[10],joydb_2[4:0]}) : joydb_1ena ? joystick_0_USB : joystick_1_USB;
+wire [31:0] joystick_0 = joydb_1ena ? (OSD_STATUS ? 32'b0 : joydb_1_mapped[6:0]) : joystick_0_USB;
+wire [31:0] joystick_1 = joydb_2ena ? (OSD_STATUS ? 32'b0 : joydb_2_mapped[6:0]) : joydb_1ena ? joystick_0_USB : joystick_1_USB;
 // [MiSTer-DB9-Pro END]
 
 hps_io #(.CONF_STR(CONF_STR)) hps_io
